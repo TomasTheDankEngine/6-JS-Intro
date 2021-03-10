f49a52cac7c5d27796ce2d8c737e40b3fbc2c97f
TomasTheDankEngine <tomas.miri@gmail.com>
2021-03-10 11:35:18 +0200

    js start

---
+++ js/01-variables.js
@@ -0,0 +1,14 @@
+const abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+console.log(abc);
+
+const abcLen = abc.length;
+
+const lastCharPos = abcLen - 1;
+
+const lastChar = abc[lastCharPos];
+
+console.log(lastChar);
+
+const abcAns = 'Is mums zinomos ' + abcLen + 'raidziu abeceles, paskutine yra: "' + lastChar + '".';
+
+console.log(abcAns);
---
+++ js/02-strings.js
@@ -0,0 +1,31 @@
+const menuo = 'Kovo';
+const diena = 'desimta';
+const daystring = 'Siandien yra: ' + menuo + ' ' + diena + '.';
+console.log(daystring);
+
+const singQuote = "This is a single (') quote."
+console.log(singQuote);
+const doubQuote = 'This is a double (") quote.';
+console.log(doubQuote);
+const bothQuotes = "These are single (') and double (\") quotes."
+console.log(bothQuotes);
+const safeQuotes = 'These are safely written single (\') and double (\") quotes.'
+console.log(safeQuotes);
+
+const navHTML = 
+`               <nav>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                </nav>`
+console.log(navHTML);
+const vardas = `uwu`;
+console.log(`"'''"'"" ${vardas} '' 'asd'""`);
+
+
+const marks = [10, 5, 8, 9, 5, 4, 2];
+const marksSentence = `Latest mark: ${marks[marks.length-1]}`;
+console.log(marksSentence);
