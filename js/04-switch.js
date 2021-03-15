bdc89df6a5c9f23f06659faf6c7f5c1e3e2701ff
TomasTheDankEngine <tomas.miri@gmail.com>
2021-03-15 11:14:48 +0200

    updates

---
+++ js/04-switch.js
@@ -0,0 +1,15 @@
+const animal = 'cato';
+
+switch (animal) {
+    case 'dog':
+        console.log('Bork');
+        break;
+    case 'cat':
+        console.log('meowee');
+        break;
+    default:
+        console.log('unknown animal');
+        break;
+}
+
+console.log('-----------');
---
+++ js/05-ternary.js
@@ -0,0 +1,14 @@
+
+const a = 5;
+const b = 7;
+
+let ats = '';
+
+if (a > b) {
+    ats = 'greater than';
+}
+else {
+    ats = 'not greater than';
+}
+
+console.log(ats);
